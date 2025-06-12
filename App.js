961e8f9bfb8c12feeb7051910db242a0feb2cf09
RajaDanish51 <rajadaanish9@gmail.com>
2025-06-12 20:55:41 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/homeScreen';
-import ProfileScreen from './screens/profileScreen';
-import SettingsScreen from './screens/settings';
+import { ApiProvider } from './context/ApiContext';
+import { SensorProvider } from './context/SensorContext';
+import ApiScreen from './screens/ApiScreen';
+import SensorScreen from './screens/SensorScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [userName, setUserName] = useState("Profile");
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name={userName}>
-          {() => <ProfileScreen userName={userName} setUserName={setUserName} />}
-        </Tab.Screen>
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApiProvider>
+      <SensorProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="API Demo" component={ApiScreen} />
+            <Tab.Screen name="Sensor Data" component={SensorScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SensorProvider>
+    </ApiProvider>
   );
 }
